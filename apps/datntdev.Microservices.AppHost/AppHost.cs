e37daed5904dea40d68469d8542db305e449e708
Dat Nguyen <datntdev@outlook.com>
2025-09-27 14:07:27 +0700

    build: add scaffold for identity, admin services

--- apps/datntdev.Microservices.AppHost/AppHost.cs
+++ apps/datntdev.Microservices.AppHost/AppHost.cs
@@ -9,4 +9,9 @@ builder.AddProject<Projects.datntdev_Microservices_Web>("webfrontend")
     .WithReference(apiService)
     .WaitFor(apiService);
 
+builder.AddProject<Projects.datntdev_Microservices_Srv_Identity_Web_Host>("srv-identity")
+    .WithHttpHealthCheck("/health");
+builder.AddProject<Projects.datntdev_Microservices_Srv_Admin_Web_Host>("srv-admin")
+    .WithHttpHealthCheck("/health");
+
 builder.Build().Run();
---
+++ services/datntdev.Microservices.Srv.Admin.Web.Host/Program.cs
@@ -0,0 +1,16 @@
+namespace datntdev.Microservices.Srv.Admin.Web.Host;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.AddServiceDefaults();
+        var app = builder.Build();
+        app.MapDefaultEndpoints();
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.Run();
+    }
+}
