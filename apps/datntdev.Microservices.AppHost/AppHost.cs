2a3588c4bbdeb3e57eaac6b7c104058d331b15db
Dat Nguyen <datntdev@outlook.com>
2025-09-27 15:51:14 +0700

    build: remove template services from starter code

--- apps/datntdev.Microservices.AppHost/AppHost.cs
+++ apps/datntdev.Microservices.AppHost/AppHost.cs
@@ -1,14 +1,5 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.datntdev_Microservices_ApiService>("apiservice")
-    .WithHttpHealthCheck("/health");
-
-builder.AddProject<Projects.datntdev_Microservices_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithHttpHealthCheck("/health")
-    .WithReference(apiService)
-    .WaitFor(apiService);
-
 builder.AddProject<Projects.datntdev_Microservices_Srv_Identity_Web_Host>("srv-identity")
     .WithHttpHealthCheck("/health");
 builder.AddProject<Projects.datntdev_Microservices_Srv_Admin_Web_Host>("srv-admin")
