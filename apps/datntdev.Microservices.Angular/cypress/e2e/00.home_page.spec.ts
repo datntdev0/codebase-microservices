56134372c553842cf8835f369568ee588076331f
Dat Nguyen <datntdev@outlook.com>
2025-11-03 21:33:06 +0700

    cypress: write authentication workflow e2e tests

--- apps/datntdev.Microservices.Angular/cypress/e2e/00.home_page.spec.ts
+++
@@ -1,6 +0,0 @@
-describe('00. Home Page - Basic Rendering and Title', () => {
-  it('should load the home page and display the correct title', () => {
-    cy.visit('/')
-    cy.title().should('equal', 'datntdev.Microservices.Angular')
-  })
-})
--- apps/datntdev.Microservices.Angular/src/shared/models/constants.ts
+++ apps/datntdev.Microservices.Angular/src/shared/models/constants.ts
@@ -1,3 +1,3 @@
 export const APPLICATION = {
-  name: 'datntdev Microservices',
+  name: 'datntdev.Microservices',
 }
